c147a47dd9f56d90ef561777c13787d28a33e0f8
Arnis Mednis <119369248+ArnisM11@users.noreply.github.com>
2023-02-20 15:12:25 +0200

    Exercises #1,#2,#10

---
+++ csharp-basics/exercises/Arithmetic/Exercise2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter number to check if it is odd or even. ====> ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(CheckOddEven(n1));
+
+            Console.ReadKey();
+        }
+        public static string CheckOddEven(int number)
+        {
+            if (number % 2 == 0) Console.WriteLine("Even Number");
+            else Console.WriteLine("Odd number");
+            return "bye!";
+        }
+    }
+}
