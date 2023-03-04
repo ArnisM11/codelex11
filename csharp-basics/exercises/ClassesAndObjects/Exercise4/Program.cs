df6868c4653e317a28560ddb4a8d6f7e654748c8
Arnis Mednis <119369248+ArnisM11@users.noreply.github.com>
2023-03-04 17:15:52 +0200

    #4 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise4/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Exercise4
+{
+    public class Movie
+    {
+        public string title;
+        public string studio;
+        public string rating;
+
+        public Movie(string title,string studio,string rating)
+        {
+            this.title = title;
+            this.studio = studio;
+            this.rating = rating;
+        }
+
+        public Movie(string title, string studio)
+        {
+            this.title = title;
+            this.studio = studio;
+            this.rating = "PG";
+        }
+
+        public Movie[] GetPG(Movie[] arrayOfMovies)
+        {
+            Movie[] result = Array.Empty<Movie>();
+            for(int i = 0; i < arrayOfMovies.Length; i++)
+            {
+                if (arrayOfMovies[i].rating == "PG")
+                {
+                    result[i] = arrayOfMovies[i];
+                }
+            }
+            return result;
+        }
+        public void PrintMovie()
+        {
+            Console.WriteLine(title + " " + studio + " " + rating);
+        }
+        static void Main(string[] args)
+        {
+            Movie casino = new Movie("Casino Royale", "Eon Productions", "PG13");
+            Movie glass = new Movie("Glass", "Buena Vista International", "PG13");
+            Movie spider = new Movie("Spider-Man: Into the Spider-Verse", "Columbia Pictures", "PG");
+            casino.PrintMovie();
+            glass.PrintMovie();
+            spider.PrintMovie();
+            Movie[] listOfMovies = new Movie[] { casino, glass, spider };
+            Console.ReadKey();
+        }
+
+    }
+}
