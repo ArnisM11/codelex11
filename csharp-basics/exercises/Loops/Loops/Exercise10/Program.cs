1258165652939834e1af2cd838a58253504eea33
Arnis Mednis <119369248+ArnisM11@users.noreply.github.com>
2023-03-04 15:05:35 +0200

    Loops done

---
+++ csharp-basics/exercises/Loops/Loops/Exercise10/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercise10
+{
+    internal class NumberSquare
+    {
+        static void Main(string[] args)
+        {
+            int min;
+            int max;
+            Console.WriteLine("Min? ");
+            min = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Max? ");
+            max = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = min; i <= max; i++)
+            {
+                for (int j = i; j <= max; j++)
+                {
+                    Console.Write(j);
+                }
+                for (int k = min; k < i; k++)
+                {
+                    Console.Write(k);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ csharp-basics/exercises/Loops/Loops/Exercise11/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercise11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseCase("Happy Birthday"));
+            Console.WriteLine(ReverseCase("MANY THANKS"));
+            Console.WriteLine(ReverseCase("sPoNtAnEoUs"));
+            Console.ReadKey();
+        }
+        static string ReverseCase(string str)
+        {
+            char[] arr = str.ToCharArray();
+            for(int i = 0; i< str.Length; i++)
+            {
+                if (Char.IsLower(arr[i]))
+                {
+                    arr[i] = Char.ToUpper(arr[i]);
+                }
+                else if (Char.IsUpper(arr[i]))
+                {
+                    arr[i] = Char.ToLower(arr[i]);
+                }
+            }
+            string result = new string(arr);
+            return result;
+        }
+    }
+}
