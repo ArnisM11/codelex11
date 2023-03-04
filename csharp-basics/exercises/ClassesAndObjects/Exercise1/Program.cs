f0c8dc68b2f772d39b3f820297d7b9d6f20c2766
Arnis Mednis <119369248+ArnisM11@users.noreply.github.com>
2023-03-04 16:56:23 +0200

    Exercise1 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Exercise1
+{
+    
+    class Product
+    {
+        public string name;
+        public double priceAtStart;
+        public int amountAtStart;
+        public Product(string name,double priceAtStart,int amountAtStart)
+        {
+            this.name = name;
+            this.priceAtStart = priceAtStart;
+            this.amountAtStart = amountAtStart;
+        }
+        public void PrintProduct()
+        {
+            Console.WriteLine(name + ", price " + priceAtStart + " , amount " + amountAtStart);
+        }
+        public void ChangeQuantity(int newAmount)
+        {
+            amountAtStart = newAmount;
+        }
+        public void ChangePrice(double newPrice)
+        {
+            priceAtStart = newPrice;
+        }
+        static void Main(string[] args)
+        {
+            Product mouse = new Product("Logitech mouse", 70.00d, 14);
+            Product phone = new Product("Iphone 5s", 999.99, 3);
+            Product printer = new Product("Epson EB-U05", 440.46, 1);
+            mouse.PrintProduct();
+            phone.PrintProduct();
+            printer.PrintProduct();
+            printer.ChangeQuantity(25);
+            printer.PrintProduct();
+            mouse.ChangePrice(55.55);
+            mouse.PrintProduct() ;
+            Console.ReadKey();
+        }
+    }
+}
