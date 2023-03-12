6e5e17afb7aaf9faa094b0d715c5bd613dcdd76a
Arnis Mednis <119369248+ArnisM11@users.noreply.github.com>
2023-03-13 00:43:41 +0200

    Revert "Prieks Arithmetics"
    This reverts commit 00fc425c3ffc0c7ef99d1a44fdc55c3e95adb3db.

---
+++ csharp-basics/exercises/Arithmetic/Exercise1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter integer nr1 ====> ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter integer nr2 ====> ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+
+            if(n1 ==15 || n2 == 15)
+            {
+                Console.WriteLine("true");
+            }
+            else if(n1+n2 == 15 || n1 - n2 ==15 || n2-n1 ==15)
+            {
+                Console.WriteLine("true");
+            }
+            else Console.WriteLine("false");
+
+            Console.ReadKey();
+        }
+    }
+}
